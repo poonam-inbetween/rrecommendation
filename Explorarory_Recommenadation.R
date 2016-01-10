4e566e750e3d339b7f21e1f87cce8d4a388afd31
Poonam Arora <peelu@Poonams-MacBook-Pro.local>
2016-01-10 15:53:54 +0800

    Exploration

--- Explorarory_Recommenadation.R
+++ Explorarory_Recommenadation.R
@@ -14,5 +14,25 @@ table_views <- data.frame(
 table_views <- table_views[order(table_views$count, decreasing = TRUE), ]
 
 
-ggplot(table_views[1:6, ], aes(x = item_names, y = count)) + geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+ggplot(table_views[1:6, ], aes(x = item_names, y = count , label = count)) + 
+  geom_bar(stat="identity") +  geom_text(vjust = -.5)
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   ggtitle("Number of Items")
+
+
+
+count_per_user <- rowCounts(ratings_matrix)
+
+table_views1 <- data.frame(
+  user_names = names(count_per_user),
+  count = count_per_user
+)
+table_views1 <- table_views1[order(table_views1$count, decreasing = TRUE), ]
+
+
+ggplot(table_views1[1:6, ], aes(x = user_names, y = count , label = count)) + 
+  geom_bar(stat="identity") +geom_text(vjust = -.5)
+theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+
+image(ratings_matrix[1:10,1:15], main = "Heatmap of the rating matrix")
